51ac97932a7f16050d49382ca6cc97d4925b84f4
Tobias Meier <Tobias.Meier@bridging-it.de>
2017-09-28 17:08:01 +0200

    Cleanup Code

---
+++ SvcFabricDinnerDemo.DinnerMenuService.Interfaces/Dish.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SvcFabricDinnerDemo.DinnerMenuService.Interfaces
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
+
+
+
+
